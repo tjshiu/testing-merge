2b8f3878a025eacc9eee39bdc98e1e022d763569
Callixte NAHIMANA <callixtenahimana@Callixtes-MacBook-Pro.local>
2018-04-22 20:44:45 -0700

    added content shorter

--- newFile.js
+++ newFile.js
@@ -3,27 +3,4 @@ $4.07 USD (0.58%)
 0.00046137 BTC (0.46%) 
 0.05473890 NEO (0.51%)
 
-Market Cap
-$458,104,335 USD 
-51,914 BTC 
-6,159,304 NEO
-Volume (24h)
-$48,718,600 USD 
-5,521 BTC 
-655,031 NEO
-Circulating Supply
-112,521,513 ONT
-Total Supply
-1,000,000,000 ONT
- Website
- Explorer
- Message Board
- Chat
- Chat 2
- Rank 49
- Token
- Charts
- Markets
- Social
- Tools
- Historical Data
+Historical Data
